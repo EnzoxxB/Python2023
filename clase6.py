b15f481ae906c30f3b970a66900f5ea0ccdebba5
Enzo Balderrama <enzoxxb@gmail.com>
2023-06-28 21:30:03 -0300

    1er commit Python

---
+++ clase6.py
@@ -0,0 +1,8 @@
+# En esta clase vemos la sentencia if/else
+condicion = False
+if condicion == True:
+    print('Condición verdadera.')
+elif condicion == False:
+    print('Condición falsa.')
+else:
+    print('Condición no especificada.')
