b047203375dab1e26d8e26df60074c10becc70a0
Enzo Balderrama <enzoxxb@gmail.com>
2023-08-15 11:19:28 -0300

    finalizando clase1

--- Segundo Semestre/leccion4.py
+++ Segundo Semestre/leccion4.py
@@ -34,3 +34,25 @@ print(nombres)
 # Eliminar todos los elementos
 nombres.clear()
 print(nombres)
+
+# Definimos una tupla (van entre paréntesis)
+cocina = ("cuchara", "cuchillo", "tenedor")
+print(len(cocina))
+
+# Acceder a un elemento
+print(cocina[0])
+print(cocina[-1]) # manera inversa
+
+# Acceder a un rango
+print(cocina[0:1])
+verduras = ("papa",)# Tupla de 1 elemento. Para que sea una tupla, aunque sea de 1 elemento debe poseer la coma.
+
+#Recorremos los elementos de la tupla.
+for cocinar in cocina:
+    print(cocinar, end=" ") # utilizamos end= para modificar como finaliza la impresión de Print(/n por default)
+
+cocinaLista = list(cocina)
+cocinaLista[0] = "Plato"
+cocina = tuple(cocinaLista)
+print("\n", cocina)
+# del cocina # para borrar la tupla
