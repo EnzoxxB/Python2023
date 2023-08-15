8b37ff42530fe0c873da7dc96810660ae0989f1a
Enzo Balderrama <enzoxxb@gmail.com>
2023-08-14 21:47:54 -0300

    Comenzando el Segundo Semestre en Python

--- Segundo Semestre/Ejercicio1.py
+++ Segundo Semestre/Ejercicio1.py
@@ -1,8 +1,6 @@
 # Ejercicio 1: Iterar un rango de 0 a 10 e imprimir números divisibles entre 3
 
-numeros = range(0,10,1)
-# numeros = range(0,10,3) // Otra forma de hacerlo
 print("Los numeros divisibles entre 3 son:")
-for num in numeros:
+for num in range(11):
     if (num%3 == 0):
         print(num)
